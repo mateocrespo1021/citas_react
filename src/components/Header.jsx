ff907584210a88cdc0d5baa6861f4b86b5034d5b
Mateo Crespo <matcre01@gmail.com>
2022-04-11 20:31:58 -0500

    No fue mucho,pero honesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <>
       <h1 className="font-black text-5xl text-center mx-auto">
-        Seguimiento Pacientes{" "}
+        Seguimiento Pacientes{" "} con Github {" "}
         <span className="text-indigo-600">Veterinaria</span>
       </h1>
     </>
